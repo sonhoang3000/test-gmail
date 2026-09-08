4caf0b7491cf84ecb6f11284c6e8a8d81c829845
sonhoang3000 <vuhoangson3000@gmail.com>
2026-09-08 09:42:10 +0700

    set up env

--- src/services/gmailService.js
+++ src/services/gmailService.js
@@ -24,12 +24,12 @@ const processedMessageIds = new Set();
 // ==========================================
 
 const oauth2Client = new google.auth.OAuth2(
-  process.env.GOOGLE_CLIENT_ID,
-  process.env.GOOGLE_CLIENT_SECRET,
+  process.env.GMAIL_CLIENT_ID,
+  process.env.GMAIL_CLIENT_SECRET,
 );
 
 oauth2Client.setCredentials({
-  refresh_token: process.env.GOOGLE_REFRESH_TOKEN,
+  refresh_token: process.env.GMAIL_REFRESH_TOKEN,
 });
 
 const gmail = google.gmail({
